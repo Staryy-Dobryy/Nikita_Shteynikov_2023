eb1e1b7590c4902f4229453e159e68ee5d938109
Staryy-Dobryy <lfcega1@gmail.com>
2023-03-08 23:33:18 +0200

    Add code for task 4

---
+++ Lesson1/task4/Program.cs
@@ -0,0 +1,11 @@
+﻿string? StrToStr(string[] strings)
+{
+    if (strings[0].Length > strings[1].Length) return string.Concat(strings[0], strings[1]);
+    else if (strings[0] == strings[1]) return null;
+    return String.Concat<string>(strings[1].Split(strings[0][0]));
+}
+string[] strings = {
+    Console.ReadLine(),
+    Console.ReadLine()
+};
+Console.WriteLine(StrToStr(strings));
