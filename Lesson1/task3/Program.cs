d0667c5f11d625a6e17ee4c96b86ed0c60e68b0a
Staryy-Dobryy <lfcega1@gmail.com>
2023-03-08 23:32:54 +0200

    Add code for task 3

---
+++ Lesson1/task3/Program.cs
@@ -0,0 +1,21 @@
+﻿List<int> people = new List<int>();
+
+Console.WriteLine("Enter 10 values: ");
+for (int i = 0; i < 10; i++)
+{
+    Console.Write(i + " - ");
+    people.Add(Convert.ToInt32(Console.ReadLine()));
+}
+
+Console.Write("Enter dublicate value: ");
+int value = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < people.Count; i++)
+{
+    Console.WriteLine(people[i]);
+    if (people[i] == value)
+    {
+        people.Insert(i++, 3);
+        Console.WriteLine(people[i]);
+    }
+}
