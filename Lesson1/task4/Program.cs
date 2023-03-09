a48927e1510b6ac1bf3e9aa3d698caea6c5cc7cb
Staryy-Dobryy <lfcega1@gmail.com>
2023-03-09 11:55:31 +0200

    Add сomments

--- Lesson1/task4/Program.cs
+++ Lesson1/task4/Program.cs
@@ -3,6 +3,8 @@
     if (strings[0].Length > strings[1].Length) return string.Concat(strings[0], strings[1]);
     else if (strings[0] == strings[1]) return null;
     return String.Concat<string>(strings[1].Split(strings[0][0]));
+    // equal to:
+    // return strings[1].Replace(strings[0][0].ToString(), "");
 }
 string[] strings = {
     Console.ReadLine(),
