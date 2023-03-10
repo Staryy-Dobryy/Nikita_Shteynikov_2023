ac880001928d07c1fb4e7881e8ebd38e56aea2c5
Staryy-Dobryy <lfcega1@gmail.com>
2023-03-10 17:22:04 +0200

    Change code

--- Lesson1/task4/Program.cs
+++ Lesson1/task4/Program.cs
@@ -1,7 +1,13 @@
-﻿string? StrToStr(string string1, string string2)
+﻿string StrToStr(string string1, string string2)
 {
-    if (string1.Length > string2.Length) return string1 + string2;
-    else if (string1 == string2) return null;
+    if (string1.Length > string2.Length)
+    {
+        return string.Concat(string1, string2);
+    }
+    if (string1 == string2)
+    {
+        return null;
+    }
     return string2.Replace(string1[0].ToString(), "");
     // equal to: String.Concat<string>(strings[1].Split(strings[0][0]));
 }
